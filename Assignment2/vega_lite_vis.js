ca7cef8906408e82993ec2dc59e5e3f2b96eadc1
megnugget <114331457+megnugget@users.noreply.github.com>
2022-10-14 11:52:44 +0800

    Add files via upload

---
+++ Assignment2/vega_lite_vis.js
@@ -0,0 +1,19 @@
+var vis1= "asgn_map.json";
+vegaEmbed('#asgn_map',vis1).then(function(result){
+
+}).catch(console.error);
+
+var vis2= "bar_chart.json";
+vegaEmbed('#bar_chart',vis2).then(function(result){
+
+}).catch(console.error);
+
+var vis3= "line_chart.json";
+vegaEmbed('#line_chart',vis3).then(function(result){
+
+}).catch(console.error);
+
+var vis4= "radial_chart.json";
+vegaEmbed('#radial_chart',vis4).then(function(result){
+
+}).catch(console.error);
